ae0b1ec2a353b3111091589c9dd80ea890726a0c
Geoffrey Unger <geoffunger@gmail.com>
2014-01-06 22:16:32 -0800

    Added new file for Google API credentials

---
+++ ad_mapper_credentials.js
@@ -0,0 +1,14 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: geoff
+ * Date: 12/18/13
+ * Time: 3:08 PM
+ * To change this template use File | Settings | File Templates.
+ *
+ * This file should be placed one directory up from the Ad Mapper web root.
+ *
+ */
+
+
+var clientId = 'YOUR-GOOGLE-CLIENT-ID';
+var apiKey = 'YOUR-GOOGLE-API-KEY';
